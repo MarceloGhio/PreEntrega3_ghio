4a8bd77a584a487902714056d003cf9e57d26131
Marce Ghio <Marce.ghio.mg@gmail.com>
2023-01-26 23:10:36 -0300

    preentrega3Ghio

---
+++ js/home.js
@@ -0,0 +1,19 @@
+const pintarProductos = (productos) => {
+    const contenedor = document.getElementById("producto-contenedor");
+
+    productos.forEach(producto => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML +=  `<div class="card-image">
+                          <img src=${producto.imagen}>
+                          <span class="card-title">${producto.nombre}</span>
+                          <a class="btn-floating wabes-effect waves-light black"><i id=${producto.id} class="material-icons agregar">add_shopping_cart</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p>${producto.desc}</p>
+                            <p>${producto.precio}</p>
+                        </div>
+                        `
+      contenedor.appendChild(div);
+    });
+  };
